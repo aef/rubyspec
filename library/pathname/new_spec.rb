c4da5365e850a12b7ad8798e001ad83112b98d73
Alexander E. Fischer <aef@raxys.net>
2011-11-17 21:07:12 +0100

    Added tests for new Pathname.[] alias to .new. Also check if old global Pathname() method warns about deprication. WIP

--- library/pathname/new_spec.rb
+++ library/pathname/new_spec.rb
@@ -17,3 +17,40 @@ describe "Pathname.new" do
 
 end
 
+describe "Pathname.[]" do
+  it "returns a new Pathname Object with 1 argument" do
+    Pathname[''].should be_kind_of(Pathname)
+  end
+
+  it "raises an ArgumentError when called with \0" do
+    lambda { Pathname["\0"]}.should raise_error(ArgumentError)
+  end
+
+  it "is tainted if path is tainted" do
+    path = '/usr/local/bin'.taint
+    Pathname[path].tainted?.should == true
+  end
+end
+
+describe "Kernel#Pathname()" do
+  it "returns a new Pathname Object with 1 argument but warns about deprecation" do
+    lambda {
+      Pathname('').should be_kind_of(Pathname)
+    }.should complain('Kernel#Pathname is deprecated; use Pathname.[] instead')
+
+  end
+
+  it "raises an ArgumentError when called with \0 but warns about deprecation" do
+    lambda {
+      lambda { Pathname("\0")}.should raise_error(ArgumentError)
+    }.should complain('Kernel#Pathname is deprecated; use Pathname.[] instead')
+  end
+
+  it "is tainted if path is tainted but warns about deprecation" do
+    lambda {
+      path = '/usr/local/bin'.taint
+      Pathname(path).tainted?.should == true
+    }.should complain('Kernel#Pathname is deprecated; use Pathname.[] instead')
+  end
+
+end
